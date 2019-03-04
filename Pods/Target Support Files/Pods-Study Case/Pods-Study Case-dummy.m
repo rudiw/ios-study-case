a400c13a2295def04b706b050b791f8051fac8a8
rudiwijaya <rudi.wijaya@mitrais.com>
2019-03-04 13:32:51 +0700

    login form

---
+++ Pods/Target Support Files/Pods-Study Case/Pods-Study Case-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_Study_Case : NSObject
+@end
+@implementation PodsDummy_Pods_Study_Case
+@end
---
+++ Study Case/VCLogin.swift
@@ -0,0 +1,18 @@
+//
+//  VCLogin.swift
+//  Study Case
+//
+//  Created by Rudi Wijaya on 04/03/19.
+//  Copyright © 2019 Rudi Wijaya. All rights reserved.
+//
+
+import UIKit
+
+
+class VCLogin: UIViewController {
+    
+    override func viewDidLoad() {
+        print("view did load");
+    }
+    
+}
